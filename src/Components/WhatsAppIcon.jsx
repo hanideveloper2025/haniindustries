12d09451cfe0058884368da3c7f6d8c2dbb5825d
Ajaykumar8j3heiwjv <smilyboy2207@gmail.com>
2025-12-22 09:13:58 +0530

    h122

---
+++ src/Components/WhatsAppIcon.jsx
@@ -0,0 +1,19 @@
+import './WhatsAppIcon.css';
+
+const WhatsAppIcon = () => {
+  return (
+    <a
+      href="https://wa.me/919342708080"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="whatsapp-icon"
+    >
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg"
+        alt="WhatsApp"
+      />
+    </a>
+  );
+};
+
+export default WhatsAppIcon;
